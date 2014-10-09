4aedf7b2d8a3ee85fd1430086e8d8afa280fe2c2
jagomez <RedNet@openmailbox.org>
2014-10-09 13:49:10 +0200

    Último gráfico

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@
 ## plot4.R that does the following:
 ## 1. Read Data and subset it
 ## 2. Open Graphic Device PNG
-## 3. Create plot
+## 3. Create plots
 ##
 ##################################################################################
 
@@ -28,10 +28,18 @@ subdata$Datetime <- as.POSIXct(DateTime)
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
 
 ##Generates the plot
+par(mfrow=c(2,2))
+## plot1
+with(subdata, plot(Global_active_power~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+##plot2
+with(subdata, plot(Voltage~Datetime, type="l", ylab="Voltage", xlab="datetime"))
+##plot3
 with(subdata, { plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
                 lines(Sub_metering_2~Datetime,col='Red')
                 lines(Sub_metering_3~Datetime,col='Blue')})
-legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1)
+legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1, bty="n")
+##plot4
+with(subdata, plot(Global_reactive_power~Datetime, type="l", ylab="Global_reactive_power", xlab="datetime"))
 
 ##Close Graphic Device
 dev.off()
