1ee8964878f764c58d712fabfe4a6fc3100afeb3
jagomez <RedNet@openmailbox.org>
2014-10-09 11:44:06 +0200

    arrancamos

---
+++ plot1.R
@@ -0,0 +1,15 @@
+##################################################################################
+## Exploratory Data Analysis
+## by Jeff Leek, PhD, Brian Caffo, PhD, Roger D. Peng, PhD
+## 
+## plot1.R by JAGomez
+## 09/10/2014
+##
+## plot1.R that does the following:
+## 1.
+## 2. 
+## 3.
+##
+##################################################################################
+
+## Read data from files
