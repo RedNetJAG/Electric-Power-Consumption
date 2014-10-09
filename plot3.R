f1cd31489e507c85c951abf48d58b1baae5b4531
jagomez <RedNet@openmailbox.org>
2014-10-09 13:25:58 +0200

    Tercer gráfico

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@
 ## plot1.R by JAGomez
 ## 09/10/2014
 ##
-## plot1.R that does the following:
+## plot3.R that does the following:
 ## 1. Read Data and subset it
 ## 2. Open Graphic Device PNG
 ## 3. Create plot
@@ -25,11 +25,13 @@ DateTime <- paste(subdata$Date, subdata$Time)
 subdata$Datetime <- as.POSIXct(DateTime)
 
 ## Open Graphic Device PNG
-png(filename = "plot2.png", width = 480, height = 480, units = "px")
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
 
 ##Generates the plot
-
-
+with(subdata, { plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
+                lines(Sub_metering_2~Datetime,col='Red')
+                lines(Sub_metering_3~Datetime,col='Blue')})
+legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1)
 
 ##Close Graphic Device
 dev.off()
