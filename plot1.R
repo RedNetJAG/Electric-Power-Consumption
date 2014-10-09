7dc486a230b23aafbaed89b2b0509a65a0cd5aac
jagomez <RedNet@openmailbox.org>
2014-10-09 12:28:15 +0200

    fin parte 1

--- plot1.R
+++ plot1.R
@@ -13,3 +13,19 @@
 ##################################################################################
 
 ## Read data from files
+data <- read.csv("../exdata_data_household_power_consumption/data.txt", sep=';', na.strings="?", header=T)
+
+## Date format and Subsetting the data
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subdata <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data)
+
+## Open Graphic Device PNG
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+##Generates the plot
+hist(subdata$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab="Frequency", col = "Red")
+
+##Close Graphic Device
+dev.off()
+
